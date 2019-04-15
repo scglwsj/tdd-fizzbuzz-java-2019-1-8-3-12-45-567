6e5f36055c3d00cfc114928f3842132418b65e1b
wsj <scglwsj@163.com>
2019-04-15 16:37:05 +0800

    重构

--- src/main/java/com/thoughtworks/tdd/Dependency.java
+++ src/main/java/com/thoughtworks/tdd/Dependency.java
@@ -6,12 +6,42 @@ public class Dependency {
     }
 
     public String fizzBuzz(Integer i) {
-        String result = "";
-
-        if (i % 3 == 0) result += "Fizz";
-        if (i % 5 == 0) result += "Buzz";
-        if (i % 7 == 0) result += "Whizz";
-
+        FizzManager manager = new FizzManager();
+        String result = manager.getString(i);
         return result.isEmpty() ? i.toString() : result;
     }
 }
+
+class FizzManager {
+    private FizzConverter converter3;
+    private FizzConverter converter5;
+    private FizzConverter converter7;
+
+    FizzManager() {
+        converter3 = new FizzConverter(3, "Fizz");
+        converter5 = new FizzConverter(5, "Buzz");
+        converter7 = new FizzConverter(7, "Whizz");
+    }
+
+    String getString(Integer i) {
+        return converter3.getConverter(i)
+                + converter5.getConverter(i)
+                + converter7.getConverter(i);
+    }
+}
+
+class FizzConverter {
+    private Integer divisor;
+    private String converter;
+
+    FizzConverter(Integer divisor, String converter) {
+        this.divisor = divisor;
+        this.converter = converter;
+    }
+
+    String getConverter(Integer dividend) {
+        return dividend % divisor == 0 ? converter : "";
+    }
+}
+
+
