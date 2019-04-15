49bccc4e268402dc2a693137f51c9b2be7cbf03b
wsj <scglwsj@163.com>
2019-04-15 16:06:53 +0800

    [Development] 测试14

--- src/main/java/com/thoughtworks/tdd/Dependency.java
+++ src/main/java/com/thoughtworks/tdd/Dependency.java
@@ -6,7 +6,9 @@ public class Dependency {
     }
 
     public String fizzBuzz(Integer i) {
-        if (i % 3 == 0) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            return "FizzBuzz";
+        } else if (i % 3 == 0) {
             return "Fizz";
         } else if (i % 5 == 0) {
             return "Buzz";
