fb55d07f492fec9f234ff61b40be0a15def6d879
wsj <scglwsj@163.com>
2019-04-15 16:18:41 +0800

    重构

--- src/main/java/com/thoughtworks/tdd/Dependency.java
+++ src/main/java/com/thoughtworks/tdd/Dependency.java
@@ -8,24 +8,10 @@ public class Dependency {
     public String fizzBuzz(Integer i) {
         String result = "";
 
-        if (i % 3 == 0) {
-            result += "Fizz";
-        }
+        if (i % 3 == 0) result += "Fizz";
+        if (i % 5 == 0) result += "Buzz";
+        if (i % 7 == 0) result += "Whizz";
 
-        if (i % 5 == 0) {
-            result += "Buzz";
-
-        }
-
-        if (i % 7 == 0) {
-            result += "Whizz";
-
-        }
-
-        if (result.isEmpty()) {
-            return i.toString();
-        } else {
-            return result;
-        }
+        return result.isEmpty() ? i.toString() : result;
     }
 }
